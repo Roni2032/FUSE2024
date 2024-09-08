9adde75a0eec0e18bd9f33b5d242840906b7ed81
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-09-08 17:33:31 +0900

    更新

--- Assets/Clear/ScoreCount.cs
+++ Assets/Clear/ScoreCount.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCount : MonoBehaviour
 {
     string a;
+    [SerializeField] TextMeshProUGUI m_TextMeshPro;
     // Start is called before the first frame update
     void Start()
     {
         int test = ScoreManager.totalScore;
         a = test.ToString();
-
+        m_TextMeshPro.text = a;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<TextMeshPro>().text = a;
     }
 }
