bd543033af4af3989694f6b75db78ff832defc15
Munakata Haruto <zeal23420039@fsgcl.jp>
2024-09-08 12:36:22 +0900

    gimmickManagerを変更

--- Assets/Main/GimmickManager.cs
+++ Assets/Main/GimmickManager.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class GimmickManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject[] gimmickObjects;
+
+    GameObject putObject;
+
+
     void Start()
     {
         
@@ -13,6 +17,38 @@ public class GimmickManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (putObject != null)
+        {
+            putObject.transform.position = GetMousePosition();
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(gimmickObjects[0], GetMousePosition(), Quaternion.identity);
+            }
+        }
+        if (Input.GetMouseButtonDown(1) && putObject == null)
+        {
+            putObject = Instantiate(gimmickObjects[0], GetMousePosition(), Quaternion.identity);
+            for (int i = 0; i < putObject.transform.childCount; i++)
+            {
+                putObject.transform.GetChild(i).GetComponent<BoxCollider>().isTrigger = true;
+        }
+        }
+    }
+
+    Vector3 GetMousePosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10.0f;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        worldPosition.z = 0.0f;
+        return worldPosition;
+    }
+
+    void PushPreviewGimmickButton(int num)
+    {
+        if(num >= gimmickObjects.Length)
+        {
+            num = gimmickObjects.Length - 1;
+        }
     }
 }
